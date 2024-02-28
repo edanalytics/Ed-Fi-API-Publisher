d30a32ceb74c864a817a8aee6d0d39820f5dcd01
ea-mtenhoor <mtenhoor@edanalytics.org>
2024-02-28 12:13:58 -0600

    Add Destructurama

--- src/EdFi.Tools.ApiPublisher.Cli/Program.cs
+++ src/EdFi.Tools.ApiPublisher.Cli/Program.cs
@@ -20,6 +20,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Destructurama;
 
 namespace EdFi.Tools.ApiPublisher.Cli
 {
@@ -308,6 +309,7 @@ namespace EdFi.Tools.ApiPublisher.Cli
                .ReadFrom.Configuration(loggerConfig)
                .Enrich.WithThreadId()
                .Enrich.FromLogContext()
+               .Destructure.JsonNetTypes()
                .CreateLogger();
         }
     }
